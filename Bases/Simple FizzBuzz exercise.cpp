b42b74d1bf578486bed162065770c5a065a4036a
Felipe_Estevanatto <69355764+FelipeEstevanatto@users.noreply.github.com>
2021-01-26 14:37:28 -0300

    file added
    now using English as the main language

---
+++ Bases/Simple FizzBuzz exercise.cpp
@@ -0,0 +1,28 @@
+/* FizzBuzz exercose
+Write a short program that prints each number from 1 to 100 on a new line. 
+
+For each multiple of 3, print "Fizz" instead of the number. 
+
+For each multiple of 5, print "Buzz" instead of the number. 
+
+For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
+
+ex output: 1, 2, Fizz, 4, Buzz, Fizz, 7, 8...
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	for(int i=1;i<101;i++){
+		if(i%3==0 & i%5==0)
+			cout << "FizzBuzz\n" ;
+		else if(i%3==0)
+			cout << "Fizz\n" ;
+		else if(i%5==0)
+			cout << "Buzz\n" ;
+		else
+			cout << i << "\n";
+	}
+	return 0;
+}
